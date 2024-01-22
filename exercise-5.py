a3b38ec3c4d58240f4af76612a35257e9fc36e20
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-22 18:11:53 +0800

    Pseudocode

--- exercise-5.py
+++ exercise-5.py
@@ -1,1 +1,13 @@
 # Check if the first and last number of a list is the same
+
+# Function: check_first_last_same
+    # Input: list of numbers
+    # Output: result (True or False)
+
+# Get the first number from the list
+
+# Get the last number from the list
+
+# Check if the first and last numbers are the same
+
+# If same, return True; otherwise, return False
